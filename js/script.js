3127aa0c494573896e609e0811022e021ecc401e
hebi26 <veleine.lucas@gmail.com>
2019-11-03 22:53:27 +0100

    add soundcloud embed
    add soundcloud embed

--- js/script.js
+++ js/script.js
@@ -46,111 +46,111 @@ $('.link').click(function(){
     // ==============VISUALIZER AUDIO=================
 
     // Establish all variables that your Analyser will use
-    var audioSource, audioIndex = 0, canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height, played;
-
-    // Create a new instance of an audio object and adjust some of its properties
-    var audio = new Audio();
-    audio.src = 'sound/générique.mp3';
-    audio.controls = true;
-    audio.loop = false;
-    audio.autoplay = false;
-
-    audio.addEventListener('play', function(){
-        played = true;
-        context.resume();
-        $("#playlist li i").remove();
-        $('#analyser_render').css({"backgroundImage" : "none"});
-        $('.current-song').append('<i class="material-icons left">play_arrow</i>');
-    });
-
-    audio.addEventListener('pause', function(){
-        played = false;
-        $('#analyser_render').css({"backgroundImage" : "url('img/digital5.gif')"});
-    });
-
-    // EVENT CHANGE
-
-    $("#playlist li").click(function(e) {
-        e.preventDefault();
-
-        audioSource = $('a', this).attr('href');
-        audio.src = audioSource;
-
-        $("#playlist li").removeClass("current-song");
-        $(this).addClass("current-song");
-        $('.current-song').append('<i class="material-icons left">play_arrow</i>');
-
-        audio.play();
-    });
-    // click CANVAS
-
-    $('#analyser_render').click(function(){
-        if(played){
-            audio.pause();
-        }
-        else{
-            context.resume();
-            audio.play();
-        }
-    });
-
-    //ON END
-
-    audio.addEventListener("ended", function(){
-        audioIndex++;
-        if(audioIndex == $("#playlist li").length){
-            audioIndex = 0;
-        }
-
-        $("#playlist li").removeClass("current-song");
-        $("#playlist li:eq("+audioIndex+")").addClass("current-song");
-        $('.current-song').append('<i class="material-icons left">play_arrow</i>');
-        audio.src = $('.current-song').children('a').attr('href');
-        audio.play();
-    });
-
-
-// Initialize the MP3 player after the page loads all of its HTML into the window
-    function initMp3Player(){
-
-        document.getElementById('audio-box').appendChild(audio);
-
-        var AudioContext = window.AudioContext;
-        canvas = document.getElementById('analyser_render');
-        context = new AudioContext();
-
-        analyser = context.createAnalyser(); // AnalyserNode method
-        ctx = canvas.getContext('2d');
-
-        // Re-route audio playback into the processing graph of the AudioContext
-        source = context.createMediaElementSource(audio);
-        source.connect(analyser);
-        analyser.connect(context.destination);
-        frameLooper();
-    }
-
-    // frameLooper() animates any style of graphics you wish to the audio frequency
-    // Looping at the default frame rate that the browser provides(approx. 60 FPS)
-    function frameLooper(){
-        window.requestAnimationFrame(frameLooper);
-        fbc_array = new Uint8Array(analyser.frequencyBinCount);
-        analyser.getByteFrequencyData(fbc_array);
-        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-        ctx.fillStyle = '#10b4fb'; // Color of the bars
-        bars = 100;
-
-        for (var i = 0; i < bars; i++) {
-            bar_x = i * 3;
-            bar_width = 2;
-            bar_height = -(fbc_array[i] / 2);
-
-            //  fillRect( x, y, width, height ) // Explanation of the parameters below
-            ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
-        }
-    }
-
-
-    initMp3Player();
+//     var audioSource, audioIndex = 0, canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height, played;
+//
+//     // Create a new instance of an audio object and adjust some of its properties
+//     var audio = new Audio();
+//     audio.src = 'sound/générique.mp3';
+//     audio.controls = true;
+//     audio.loop = false;
+//     audio.autoplay = false;
+//
+//     audio.addEventListener('play', function(){
+//         played = true;
+//         context.resume();
+//         $("#playlist li i").remove();
+//         $('#analyser_render').css({"backgroundImage" : "none"});
+//         $('.current-song').append('<i class="material-icons left">play_arrow</i>');
+//     });
+//
+//     audio.addEventListener('pause', function(){
+//         played = false;
+//         $('#analyser_render').css({"backgroundImage" : "url('img/digital5.gif')"});
+//     });
+//
+//     // EVENT CHANGE
+//
+//     $("#playlist li").click(function(e) {
+//         e.preventDefault();
+//
+//         audioSource = $('a', this).attr('href');
+//         audio.src = audioSource;
+//
+//         $("#playlist li").removeClass("current-song");
+//         $(this).addClass("current-song");
+//         $('.current-song').append('<i class="material-icons left">play_arrow</i>');
+//
+//         audio.play();
+//     });
+//     // click CANVAS
+//
+//     $('#analyser_render').click(function(){
+//         if(played){
+//             audio.pause();
+//         }
+//         else{
+//             context.resume();
+//             audio.play();
+//         }
+//     });
+//
+//     //ON END
+//
+//     audio.addEventListener("ended", function(){
+//         audioIndex++;
+//         if(audioIndex == $("#playlist li").length){
+//             audioIndex = 0;
+//         }
+//
+//         $("#playlist li").removeClass("current-song");
+//         $("#playlist li:eq("+audioIndex+")").addClass("current-song");
+//         $('.current-song').append('<i class="material-icons left">play_arrow</i>');
+//         audio.src = $('.current-song').children('a').attr('href');
+//         audio.play();
+//     });
+//
+//
+// // Initialize the MP3 player after the page loads all of its HTML into the window
+//     function initMp3Player(){
+//
+//         document.getElementById('audio-box').appendChild(audio);
+//
+//         var AudioContext = window.AudioContext;
+//         canvas = document.getElementById('analyser_render');
+//         context = new AudioContext();
+//
+//         analyser = context.createAnalyser(); // AnalyserNode method
+//         ctx = canvas.getContext('2d');
+//
+//         // Re-route audio playback into the processing graph of the AudioContext
+//         source = context.createMediaElementSource(audio);
+//         source.connect(analyser);
+//         analyser.connect(context.destination);
+//         frameLooper();
+//     }
+//
+//     // frameLooper() animates any style of graphics you wish to the audio frequency
+//     // Looping at the default frame rate that the browser provides(approx. 60 FPS)
+//     function frameLooper(){
+//         window.requestAnimationFrame(frameLooper);
+//         fbc_array = new Uint8Array(analyser.frequencyBinCount);
+//         analyser.getByteFrequencyData(fbc_array);
+//         ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+//         ctx.fillStyle = '#10b4fb'; // Color of the bars
+//         bars = 100;
+//
+//         for (var i = 0; i < bars; i++) {
+//             bar_x = i * 3;
+//             bar_width = 2;
+//             bar_height = -(fbc_array[i] / 2);
+//
+//             //  fillRect( x, y, width, height ) // Explanation of the parameters below
+//             ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+//         }
+//     }
+//
+//
+//     initMp3Player();
 
     // ------------------------FUNCTION SKILLS-------------------------
 
